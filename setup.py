7c2df037fae3061375b2396cfda44c4a5cc5429a
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-08-26 10:33:59 +0300

    Self stupidity fix

--- setup.py
+++ setup.py
@@ -1,44 +1,13 @@
 #!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 
 # django-opensearch
 # setup.py
 
 
-from setuptools import setup, find_packages
+import setuptools
 
 
-# metadata
-VERSION = (0, 4, 2)
-__version__ = ".".join(map(str, VERSION))
-
-setup(
-    name="django-opensearch",
-    version=__version__,
-    packages=find_packages(),
-    install_requires=["Django"],
-    author="Alexei Andrushievich",
-    author_email="vint21h@vint21h.pp.ua",
-    description="Handle opensearch.xml",
-    license="GPLv3 or later",
-    url="https://github.com/vint21h/django-opensearch/",
-    download_url="https://github.com/vint21h/django-opensearch/archive/{version}.tar.gz".format(
-        version=__version__
-    ),
-    zip_safe=False,
-    include_package_data=True,
-    classifiers=[
-        "Environment :: Plugins",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Topic :: Utilities",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.9",
-        "Framework :: Django :: 1.10",
-        "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
